fa85d171c0a64e970876d44a1ea2aff822655208
Ersin Samur <ersin_samur@yahoo.com>
2023-05-11 14:54:30 +0100

    more tests are added

--- cypress/e2e/2-advanced-examples/testStructure.cy.js
+++ cypress/e2e/2-advanced-examples/testStructure.cy.js
@@ -21,4 +21,20 @@ describe('Context: My first test case',()=>{
         cy.visit('/registration_form');
       
     })
+    it ('Test2', ()=>{
+        expect(false).to.equal(false);
+
+    })
+    it ('Test3', ()=>{
+        expect(false).not.to.equal(true);
+        
+    })
+    it ('Test4', ()=>{
+        expect(5).to.equal(5);
+        
+    })
+    it ('Test5', ()=>{
+        expect(true).to.equal('5'==5);
+        
+    })
 })
